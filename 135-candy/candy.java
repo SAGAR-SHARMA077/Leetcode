f31f22916b5ec1a1224d49ea0be52844b7bcba36
SAGAR-SHARMA077 <sagarsharma709200@gmail.com>
2026-09-05 01:12:12 +0530

    Time: 3 ms (86.58%) | Memory: 52.5 MB (6.82%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] c= new int[ratings.length];
+        for(int i=0;i<ratings.length;i++){
+            c[i]=1;
+        }
+
+        for(int i=1;i<ratings.length;i++){
+            if(ratings[i]>ratings[i-1]){
+                c[i]=c[i-1]+1;
+            }
+        }
+
+        for(int i=ratings.length-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                c[i]=Math.max(c[i+1]+1,c[i]);
+            }
+        }
+        int s=0;
+        for(int i=0;i<c.length;i++){
+            s=s+c[i];
+        }
+        return s;
+    }
+}
